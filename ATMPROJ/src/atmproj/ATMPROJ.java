2511ead82adf512d8a017ffd4737cd240b2f2b90
Borhm99 <ibrahimisma3il@icloud.com>
2023-07-31 19:14:32 +0300

    Adding ATM interfacr project using java application

---
+++ ATMPROJ/src/atmproj/ATMPROJ.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atmproj;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author ibrahim ismail
+ */
+public class ATMPROJ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame home = new HOME();
+        home.setVisible(true);
+//        50607080902211
+        
+    }
+    
+}
