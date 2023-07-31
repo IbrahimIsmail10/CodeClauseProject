09a3a4536b95db3a42035a96c1ddeb22acab9b46
Borhm99 <ibrahimisma3il@icloud.com>
2023-07-31 18:45:13 +0300

    this is the atm interface project using java application

---
+++ ATMPROJ1/src/main/java/com/mycompany/atmproj/ATMPROJ.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.atmproj;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author ibrahim ismail
+ */
+public class ATMPROJ {
+    public static void main(String args[]){
+        JFrame home = new HOME();
+        home.setVisible(true);
+    }
+    
+}
